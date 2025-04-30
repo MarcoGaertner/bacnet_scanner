02d9534b0f7161afc696063820a1f3098e81d632
Gaertner <marco-paul.gaertner@siemens.com>
2025-04-30 09:59:50 +0200

    added gui

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from bacnet_scanner import scan_bacnet
-
 def main():
-    print("Starting BACnet Scanner...")
-    scan_bacnet()
-    print("Scan finished.")
+    print("BACnet Scanner")
+    print("1. Konsolen-Version")
+    print("2. GUI-Version")
+    
+    while True:
+        choice = input("\nBitte wählen Sie (1/2): ")
+        if choice == "1":
+            from bacnet_scanner import scan_bacnet
+            from ip_selector import select_ip
+            from port_selector import select_port
+            
+            selected_ip = select_ip()
+            selected_port = select_port()
+            scan_bacnet(selected_ip, selected_port)
+            break
+            
+        elif choice == "2":
+            from gui import start_gui
+            start_gui()
+            break
+            
+        else:
+            print("Ungültige Eingabe! Bitte 1 oder 2 wählen.")
 
 if __name__ == "__main__":
     main()
